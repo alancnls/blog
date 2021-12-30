59fa66cb7fa38408fc7684c108bacf96d474fa75
Alan Canales <alan@arcanales.mx>
2021-12-30 09:59:06 -0600

    today

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,37 +2,37 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | Alan Canales"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Alan Canales"
   end
 
   test "should get blog" do
-    get static_pages_blog_url
+    get blog_path
     assert_response :success
     assert_select "title", "Blog | Alan Canales"
   end
 
   test "should get bookshelf" do
-    get static_pages_bookshelf_url
+    get bookshelf_path
     assert_response :success
     assert_select "title", "Bookshelf | Alan Canales"
   end
 
   test "should get people" do
-    get static_pages_people_url
+    get people_path
     assert_response :success
     assert_select "title", "People | Alan Canales"
   end
 
   test "should get work" do
-    get static_pages_work_url
+    get work_path
     assert_response :success
     assert_select "title", "Work | Alan Canales"
   end
